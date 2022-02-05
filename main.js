7e1e533544382007b6ecba0db47afc792f5f525a
Wivon <brousselilian6@gmail.com>
2022-02-05 08:31:09 +0100

    🛠️working on nav & UI

--- main.js
+++ main.js
@@ -6,32 +6,32 @@ const path = require('path');
 let mainWindow;
 
 function createWindow() {
-  mainWindow = new BrowserWindow({
-    width: 1040,
-    height: 655,
-    resizable: true,
-    backgroundColor: '#1f1f1f',
-    webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: false,
-      enableRemoteModule: true,
-    },
-    // icon: path.join(__dirname, 'src/img/logoX512.png')
-  });
+    mainWindow = new BrowserWindow({
+        width: 1040,
+        height: 655,
+        resizable: true,
+        backgroundColor: '#1f1f1f',
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+            enableRemoteModule: true,
+        },
+        // icon: path.join(__dirname, 'src/img/logoX512.png')
+    });
 
-  mainWindow.loadFile('front_ui/index.html');
-  mainWindow.on('closed', function () {
-    mainWindow = null;
-  });
+    mainWindow.loadFile('front_ui/index.html');
+    mainWindow.on('closed', function () {
+        mainWindow = null;
+    });
+
+    // open dev tools 
+    mainWindow.webContents.openDevTools()
+
+    // hide menubar  
+    mainWindow.setMenu(null)
 }
 
 app.on('ready', () => {
     createWindow()
     console.log(`app version: ${app.getVersion()}`)
-  
-    globalShortcut.register('Alt+CommandOrControl+I', () => {
-      mainWindow.webContents.send('goto-main-panel')
-      mainWindow.show()
-      console.log('app maximazed')
-    })
 });
